be12e9951b56421955e1525ae5f91b964714724b
rivni <robottohakase@gmail.com>
2022-05-19 00:40:03 +0900

    kaimonolist

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import  { ref, computed } from "vue";
+
+const count = ref(0);
+const countMessage = computed(() => "回数: " + count.value);
+</script>
+
+<template>
+    <div>{{ countMessage }}</div>
+    <button @click="count++">クリック！</button>
+    <button @click="count = count * count">押すな！</button>
+    <button @click="count = 0">リセット！</button>
+</template>
+
+<style></style>
